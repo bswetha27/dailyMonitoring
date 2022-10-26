6e49d580623698f904c5487860c487c59e4ff567
bswetha27@gmail.com <bswetha27@gmail.com>
2022-10-26 13:56:40 -0500

    updated more scenarios

--- DailyMonitoring_Automation/src/main/resources/features/sapient/hrsa/demographic/demographicTitlePage.feature
+++ DailyMonitoring_Automation/src/main/resources/features/sapient/hrsa/demographic/demographicTitlePage.feature
@@ -9,4 +9,66 @@ Feature: Check demographic page loaded
     Then validate logo of the page
     And validate Title of the page
     And validate paragraph text
+    And quit
+    
+    
+    @DataRefreshment
+    Scenario: Validating the data Refreshment 
+    Given load the demographic page url
+    When demographic page is loaded
+    Then validate the date
+    And validate the About this Dashboard PDF
+    And quit
+    
+    @Dashboard
+    Scenario: Verifying the Explore Demographics header
+    Given load the demographic page url
+    When demographic page is loaded
+    Then Verify the header
+    And Verify the dashboard
+    And quit
+    
+    @AboutHRSA
+    Scenario: Verifying the About HRSA header
+    Given load the demographic page url
+    When demographic page is loaded
+    Then Verify the About HRSA header
+    And Verify the Paragraph
+    And Verify the learn more about HRSA link
+    And quit
+    
+    @FooterLinks
+    Scenario: Validating the footer link About us
+    Given load the demographic page url
+    Then About us
+    And quit
+    
+    Scenario: Validating the footer link About the data
+    Given load the demographic page url
+    Then About the data
+    And quit
+    
+    Scenario: Validating the footer link A-Z index
+    Given load the demographic page url
+    Then A-Z index
+    And quit
+    
+    Scenario: Validating the footer link Site map
+    Given load the demographic page url
+    Then Site map
+    And quit
+    
+    Scenario: Validating the footer link What's new
+    Given load the demographic page url
+    Then Site map
+    And quit
+    
+    @FooterLogos
+    Scenario: Verifying the footer images
+    Given load the demographic page url
+    When demographic page is loaded
+    Then Verify HRSA logo
+    And Verify Department of health and human serivces logo
+    And quit
+    
 
--- DailyMonitoring_Automation/src/test/java/com/sapient/hrsa/automation/stepDefinition/demographic/DemographicPage.java
+++ DailyMonitoring_Automation/src/test/java/com/sapient/hrsa/automation/stepDefinition/demographic/DemographicPage.java
@@ -50,5 +50,84 @@ public class DemographicPage {
 	public void validate_paragraph_text() {
 		driver.findElement(By.xpath("//h1[@class='text-title page-entry-title']" )).getText();
 	}
+	
+	@Then("validate the date")
+	public void validate_the_date() {
+		driver.findElement(By.xpath("//div[@class='dashboard-text']" )).getText();
+	}
 
+	@Then("validate the About this Dashboard PDF")
+	public void validate_the_about_this_dashboard_pdf() {
+		 driver.findElement(By.xpath("//a[@href='/Content/Documents/topics/About the Demographics Dashboard.pdf']" )).click();
+	}
+	
+	
+		@Then("Verify the header")
+		public void verify_the_header() {
+			driver.findElement(By.xpath("//h2[contains(text(),'Explore Demographics')]" )).getText();
+		    
+		}
+
+		@Then("Verify the dashboard")
+		public void verify_the_dashboard() {
+			driver.findElement(By.xpath("//div[@id='vizContainer']" )).getText();
+		   
+		}
+		
+		@Then("Verify the About HRSA header")
+		public void verify_the_about_hrsa_header() {
+			driver.findElement(By.xpath("//h2[contains(text(),'About HRSA')]")).getText();
+		}
+
+		@Then("Verify the Paragraph")
+		public void verify_the_paragraph() {
+			driver.findElement(By.xpath("//div[@class='footer-hrsa-content']")).getText();
+		}
+
+		@Then("Verify the learn more about HRSA link")
+		public void verify_the_learn_more_about_hrsa_link() {
+			driver.findElement(By.xpath("//a[@href='https://www.hrsa.gov']" )).click();
+		}
+		
+		@Then("About us")
+		public void about_us() {
+			 driver.findElement(By.xpath("//a[@title='About Us']" )).click();
+		}
+
+		@Then("About the data")
+		public void about_the_data() {
+			driver.findElement(By.xpath("//ul[@class='footer-links']//a[@title='About the Data']")).click();
+		}
+
+		@Then("A-Z index")
+		public void a_z_index() {
+			 driver.findElement(By.xpath("//ul[@class='footer-links']//a[@title='A to Z Index']")).click();
+		}
+
+		@Then("Site map")
+		public void site_map() {
+			driver.findElement(By.xpath("//ul[@class='footer-links']//a[@title='Site Map']")).click();
+		}
+
+		@Then("What's new")
+		public void what_s_new() {
+			driver.findElement(By.xpath("//a[@title='Whats New']")).click();
+		    
+		}
+
+		@Then("Verify HRSA logo")
+		public void verify_hrsa_logo() {
+			driver.findElement(By.xpath("//img[@src='/Content/Images/hrsa-data-warehouse-logo.png']"
+            		)).isDisplayed();
+		}
+
+		@Then("Verify Department of health and human serivces logo")
+		public void verify_department_of_health_and_human_serivces_logo() {
+			driver.findElement(By.xpath("//img[@src='/Content/Images/department-of-health-and-human-services-logo.png']")).isDisplayed();		
+			}
+
+		@Then("quit")
+		public void quit() {
+		    driver.quit();
+		}
 }
